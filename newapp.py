88c1e75c109b830d0df0d25d6df1e76995e8f726
mandrewb <75001685+mandrewb@users.noreply.github.com>
2023-03-08 21:30:19 -0600

    Update newapp.py

--- newapp.py
+++ newapp.py
@@ -1,13 +1,26 @@
 import streamlit as st
-from streamlit import option_menu
 
+# 1. as sidebar menu
 with st.sidebar:
     selected = option_menu("Main Menu", ["Home", 'Settings'], 
         icons=['house', 'gear'], menu_icon="cast", default_index=1)
     selected
 
-# horizontal Menu
+# 2. horizontal menu
 selected2 = option_menu(None, ["Home", "Upload", "Tasks", 'Settings'], 
     icons=['house', 'cloud-upload', "list-task", 'gear'], 
     menu_icon="cast", default_index=0, orientation="horizontal")
+selected2
+
+# 3. CSS style definitions
+selected3 = option_menu(None, ["Home", "Upload",  "Tasks", 'Settings'], 
+    icons=['house', 'cloud-upload', "list-task", 'gear'], 
+    menu_icon="cast", default_index=0, orientation="horizontal",
+    styles={
+        "container": {"padding": "0!important", "background-color": "#fafafa"},
+        "icon": {"color": "orange", "font-size": "25px"}, 
+        "nav-link": {"font-size": "25px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},
+        "nav-link-selected": {"background-color": "green"},
+    }
+)
     
