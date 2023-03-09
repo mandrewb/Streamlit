4229183442b5a47e92ca1269cd7c2e22b404b8f5
mandrewb <75001685+mandrewb@users.noreply.github.com>
2023-03-08 21:10:35 -0600

    Update newapp.py

--- newapp.py
+++ newapp.py
@@ -1,2 +1,13 @@
+import streamlit as st
+from streamlit_option_menu import option_menu
 
-print ("hello")
+with st.sidebar:
+    selected = option_menu("Main Menu", ["Home", 'Settings'], 
+        icons=['house', 'gear'], menu_icon="cast", default_index=1)
+    selected
+
+# horizontal Menu
+selected2 = option_menu(None, ["Home", "Upload", "Tasks", 'Settings'], 
+    icons=['house', 'cloud-upload', "list-task", 'gear'], 
+    menu_icon="cast", default_index=0, orientation="horizontal")
+    selected2
